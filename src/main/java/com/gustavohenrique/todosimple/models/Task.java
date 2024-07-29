e1e236a78553cb3845658f972483139735fab006
Gustavo Henrique <gustavohenrique.atenas@gmail.com>
2024-07-29 09:26:00 -0300

    Create View

--- src/main/java/com/gustavohenrique/todosimple/models/Task.java
+++ src/main/java/com/gustavohenrique/todosimple/models/Task.java
@@ -11,15 +11,12 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = Task.TABLE_NAME)
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class Task {
     public static final String TABLE_NAME = "task";
 
@@ -30,11 +27,88 @@ public class Task {
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    @JsonBackReference
     private User user;
 
     @Column(name = "description", length = 255, nullable = false)
     @Size(min = 1, max = 255)
     @NotBlank
     private String description;
+
+    public Task(){
+
+    }
+
+    public Task(Long id, User user, String description) {
+        this.id = id;
+        this.user = user;
+        this.description = description;
+    }
+   
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Task id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public Task user(User user) {
+        setUser(user);
+        return this;
+    }
+
+    public Task description(String description) {
+        setDescription(description);
+        return this;
+    }
+
+     @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Task))
+            return false;
+        Task other = (Task) obj;
+        if (this.id == null)
+            if (other.id != null)
+                return false;
+            else if (!this.id.equals(other.id))
+                return false;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user)
+                && Objects.equals(this.description, other.description);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
 
 }
